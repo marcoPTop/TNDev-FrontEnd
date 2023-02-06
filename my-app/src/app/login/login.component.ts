d350faf11810a8feae559ab971327d2a9af23808
Leonardo Di Salvo <leonardodisalvo99@gmail.com>
2023-02-06 16:15:20 +0100

    set the form, validation and sizing

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    userName: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
